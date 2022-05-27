110bba6e18eb81596adc6b812a79862064b366f1
samirumusa <smusa87@gmail.com>
2022-05-27 09:39:07 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Route} from 'react-router-dom'
+import NavBarBoot from '../src/components/navbarMui/index'
+import HeroSection from '../src/components/hero/HeroSection'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route>
+      <NavBarBoot> </NavBarBoot>
+      <HeroSection></HeroSection>
+      </Route>
+    
     </div>
   );
 }
